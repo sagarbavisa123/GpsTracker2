b09fe171717e75eeb9dc36ace9c069bc20696a7c
sagarbavisa123 <47737547+sagarbavisa123@users.noreply.github.com>
2019-02-18 14:14:06 +0530

    Add files via upload

---
+++ CircleJoin.java
@@ -0,0 +1,18 @@
+package com.example.user.gpstrackerapp2;
+
+public class CircleJoin {
+
+
+    public String circlememberid;
+    public CircleJoin(String circlememberid)
+    {
+        this.circlememberid=circlememberid;
+    }
+
+
+    public CircleJoin()
+    {
+
+    }
+
+}
